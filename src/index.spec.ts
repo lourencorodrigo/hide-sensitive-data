b12f7dd34ef93181978aff6325a27157d75ae39b
Rodrigo Lourenço <rsilvape@gmail.com>
2023-12-13 10:16:27 -0300

    Adding new way to obfuscate data

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,87 +1,99 @@
-import { mask } from './index'
+import { mask } from "./index";
 
-describe('obfuscateData', () => {
-  it('should obfuscate email using a different pattern', () => {
-    const data = { email: 'example@gmail.com' }
+describe("obfuscateData", () => {
+  it("should obfuscate email using a different pattern", () => {
+    const data = { email: "example@gmail.com" };
     const newDate = mask(data, [
       {
-        key: 'email',
+        key: "email",
         replacement(value) {
-          const values = value.split('@')
-          return `*******@${values[1]}`
+          const values = value.split("@");
+          return `*******@${values[1]}`;
         },
       },
-    ])
-    expect(newDate).toEqual({ email: '*******@gmail.com' })
-  })
+    ]);
+    expect(newDate).toEqual({ email: "*******@gmail.com" });
+  });
 
-  it('should obfuscate email using a different pattern', () => {
-    const data = { email: 'example@gmail.com' }
+  it("should obfuscate email using a different pattern", () => {
+    const data = { email: "example@gmail.com" };
     const newDate = mask(data, [
       {
-        key: 'email',
+        key: "email",
         replacement: () => undefined,
       },
-    ])
-    expect(newDate).toEqual({ email: undefined })
-  })
+    ]);
+    expect(newDate).toEqual({ email: undefined });
+  });
 
-  it('should obfuscate email using a replacement as a function', () => {
-    const data = { email: 'example@gmail.com', cpf: '123232' }
+  it("should obfuscate email using a replacement as a function", () => {
+    const data = { email: "example@gmail.com", cpf: "123232" };
     const newDate = mask(data, [
       {
-        key: 'email',
-        replacement: () => '**',
+        key: "email",
+        replacement: () => "**",
       },
-    ])
-    expect(newDate).toEqual({ email: '**', cpf: '123232' })
-  })
+    ]);
+    expect(newDate).toEqual({ email: "**", cpf: "123232" });
+  });
 
-  it('should obfuscate email using a replacement as a string', () => {
-    const data = { email: 'example@gmail.com', cpf: '123232' }
+  it("should obfuscate email using a replacement as a string", () => {
+    const data = { email: "example@gmail.com", cpf: "123232" };
     const newDate = mask(data, [
       {
-        key: 'email',
-        replacement: '****',
+        key: "email",
+        replacement: "****",
       },
-    ])
-    expect(newDate).toEqual({ email: '****', cpf: '123232' })
-  })
+    ]);
+    expect(newDate).toEqual({ email: "****", cpf: "123232" });
+  });
 
-  it('should not obfuscate anything', () => {
-    const data = { email: 'example@gmail.com', cpf: '123232' }
-    const newDate = mask(data, [])
-    expect(newDate).toEqual(data)
-  })
+  it("should not obfuscate anything", () => {
+    const data = { email: "example@gmail.com", cpf: "123232" };
+    const newDate = mask(data, []);
+    expect(newDate).toEqual(data);
+  });
 
-  it('should validate that mask is not changing the reference of data', () => {
-    const data = { person: { name: 'Ana' } }
-    const newData = mask(data, [{key: 'person.name', replacement: '******'}])
-    expect(newData).not.toEqual(data)
-    expect(data).toEqual(data)
-  })
+  it("should validate that mask is not changing the reference of data", () => {
+    const data = { person: { name: "Ana" } };
+    const newData = mask(data, [{ key: "person.name", replacement: "******" }]);
+    expect(newData).not.toEqual(data);
+    expect(data).toEqual(data);
+  });
 
-  it('shoud not obfuscate as we are passing ignore with a value of true', () => {
-    const data = { email: 'example@gmail.com' }
+  it("shoud not obfuscate as we are passing ignore with a value of true", () => {
+    const data = { email: "example@gmail.com" };
     const newDate = mask(data, [
       {
-        key: 'email',
+        key: "email",
         ignore: true,
         replacement: () => undefined,
       },
-    ])
-    expect(newDate).toEqual(data)
-  })
+    ]);
+    expect(newDate).toEqual(data);
+  });
 
-  it('shoud obfuscate as we are passing ignore with a value of false', () => {
-    const data = { email: 'example@gmail.com' }
+  it("shoud obfuscate as we are passing ignore with a value of false", () => {
+    const data = { email: "example@gmail.com" };
     const newDate = mask(data, [
       {
-        key: 'email',
+        key: "email",
         ignore: false,
         replacement: () => undefined,
       },
-    ])
-    expect(newDate).toEqual({ email: undefined })
-  })
-})
+    ]);
+    expect(newDate).toEqual({ email: undefined });
+  });
+
+  it("shoud obfuscate as we are passing only string", () => {
+    const data = { person: { email: "example@gmail.com" } };
+    const newDate = mask(data, ["person.email"]);
+    expect(newDate).toEqual({ person: { email: "*****************" } });
+  });
+
+  it("shoud obfuscate as we are passing number or boolean", () => {
+    const data = { person: { age: 25, isBrazilian: true } };
+    const newDate = mask(data, ["person.age", "person.isBrazilian"]);
+    expect(newDate).toEqual({ person: { age: "**", isBrazilian: "*" } });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,67 @@
-import cloneDeep from 'lodash.clonedeep'
+import cloneDeep from "lodash.clonedeep";
 
 type DeepKeyOf<T> = T extends object
   ? { [K in keyof T]: K | `${K & string}.${DeepKeyOf<T[K]> & string}` }[keyof T]
   : never;
 
-type SensitiveValues<K> = Array<{
-  key: K
-  replacement: ((value: any) => any) | string
-  ignore?: boolean
-}>
+type SensitiveValues<K> = Array<
+  | {
+      key: K;
+      replacement: ((value: any) => any) | string;
+      ignore?: boolean;
+    }
+  | (K & string)
+>;
+
+function hideCharacters(value: any): string {
+  const stringValue = String(value);
+
+  if (typeof value === "string" || typeof value === "number") {
+    return "*".repeat(stringValue.length);
+  } else {
+    return "*";
+  }
+}
 
 export function mask<T, K extends DeepKeyOf<T>>(
   obj: T,
-  sensitiveValues: SensitiveValues<K>,
+  sensitiveValues: SensitiveValues<K>
 ) {
-  const newObj = cloneDeep(obj)
-  
-  sensitiveValues.forEach((sensitiveValue) => {
-    const properties: string[] = sensitiveValue.key.toString().split('.')
-    let currentObj: any = newObj
+  const newObj = cloneDeep(obj);
 
-    if (sensitiveValue.ignore) {
-      return
+  sensitiveValues.forEach((sensitiveValue) => {
+    const properties: string[] = (
+      typeof sensitiveValue === "string" ? sensitiveValue : sensitiveValue.key
+    )
+      .toString()
+      .split(".");
+    let currentObj: any = newObj;
+
+    if (typeof sensitiveValue !== "string" && sensitiveValue.ignore) {
+      return;
     }
 
     for (let i = 0; i < properties.length; i++) {
-      const property = properties[i]
+      const property = properties[i];
       if (Object.prototype.hasOwnProperty.call(currentObj, property)) {
         if (i === properties.length - 1) {
-          currentObj[property] =
-              typeof sensitiveValue.replacement === 'string'
+          let newValue;
+          if (typeof sensitiveValue === "string") {
+            newValue = hideCharacters(currentObj[property]);
+          } else {
+            newValue =
+              typeof sensitiveValue.replacement === "string"
                 ? sensitiveValue.replacement
-                : sensitiveValue.replacement(currentObj[property])
+                : sensitiveValue.replacement(currentObj[property]);
+          }
+
+          currentObj[property] = newValue;
         } else {
-          currentObj = currentObj[property]
+          currentObj = currentObj[property];
         }
       }
     }
-  })
+  });
 
-  return newObj
+  return newObj;
 }
